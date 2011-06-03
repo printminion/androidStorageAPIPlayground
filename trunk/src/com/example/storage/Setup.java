ed58ecd9a2a40f24e68e1d21498845a9f2b83f03
russenreaktor <cr@russenreaktor.de>
2011-06-03 16:26:53 +0200

    added some setup constants

--- trunk/src/com/example/storage/Setup.java
+++ trunk/src/com/example/storage/Setup.java
@@ -2,6 +2,9 @@ package com.example.storage;
 
 public class Setup {
 
-	public static String X_GOOG_PROJECT_ID = "put your x-goog-project-id here";
+	public static String CONF_X_GOOG_PROJECT_ID = "put your x-goog-project-id here";
+
+	public static String CONF_LEGACY_ACCESS_KEY = "put your legacy access key here";
+	public static String CONF_LEGACY_ACCESS_SECRET = "put your legacy access secret here";
 
 }
