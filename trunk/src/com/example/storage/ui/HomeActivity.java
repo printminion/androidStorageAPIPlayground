ccbbbb0db2912a3d6bc19dd86641cc8435767149
russenreaktor <cr@russenreaktor.de>
2011-06-03 15:41:20 +0200

    initial version

---
+++ trunk/src/com/example/storage/ui/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.storage.ui;
+
+import com.example.storage.R;
+import com.example.storage.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
